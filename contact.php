efc4fe8a6cd4798297e6bf53ec15b0e094747c78
timmy burrito <timothylalbrecht@gmail.com>
2018-08-30 18:33:30 -0400

    comment in php file

--- contact.php
+++ contact.php
@@ -1,5 +1,12 @@
 <?php
 
+/*So I jumped into this php stuff without doing my homework. im only rocking a static page right now, so php
+ain't gonna do much. A google search later, and formspree to the rescue. I set up a dummy mail account
+U:TLABootcamp@protonmail.com
+P:bootcamp
+I plan on using the above account solely for bootcamp dummy work, so feel free and verify your message goes
+through, or ask my for a super secret authentication ;)*/
+
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $taQuestion = $_POST['taQuestion'];
